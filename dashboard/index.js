8a89008bfa06ab9d928892a9f2b0e3d7273da17d
Michal Škoula <michal@exitshop.cz>
2020-02-24 21:34:54 +0100

    dashboard improvements

---
+++ dashboard/index.js
@@ -0,0 +1,51 @@
+Vue.filter('formatKilo', function(value) {
+    if (!value) {
+        return '';
+    }
+    value = parseFloat(value.toString());
+    return (value / 1000).toFixed(2);
+});
+
+var vm = new Vue({
+    el: '#app',
+    methods: {
+        updateData: function(event) {
+            // json
+            axios.get('data/ledtech.json').then(response => (
+                this.ledtech = response.data
+            ));
+            axios.get('data/lasena.json').then(response => (
+                this.lasena = response.data
+            ));
+            axios.get('data/arduino.json').then(response => (
+                this.arduino = response.data
+            ));
+            
+            // camera
+            document.getElementById("ledtech-cam").src = document.getElementById("ledtech-cam-a").href = 'data/ledtech.jpg?t=' + (new Date().getTime());
+            if (typeof this.lightbox.destroy === 'function') {
+                this.lightbox.destroy();
+            }
+            this.lightbox = new SimpleLightbox({elements: 'a.lightbox'});
+        }
+    },
+    data: {
+        ledtech: {
+            temp: {}
+        },
+        lasena: {
+            temp: {}
+        },
+        arduino: {
+            temp: {}
+        },
+        lightbox: ''
+    },
+    mounted: function() {
+        this.updateData();
+
+        window.setInterval(() => {
+            this.updateData();
+        }, 15 * 1000);
+    }
+});
