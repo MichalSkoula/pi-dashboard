9fa96e3e83b5b68fa5b536b0b3784be21d8ccbba
MichalSkoula <michal@exitshop.cz>
2020-11-12 16:07:27 +0100

    arduino rain sensor

--- arduino-sensor/arduino-sensor.ino
+++ arduino-sensor/arduino-sensor.ino
@@ -5,28 +5,36 @@
 #include <ArduinoJson.h>
 
 // config --------------------------
-// PINs
-int moisturePin = A2;
-int lightPin = A1;
+// humidity
 int humidityPin = 4;
+DHT dht11(humidityPin, DHT11);
+
+//rain
+const double WetValue = 65;
+const double DryValue = 0;
+int rainPowerPin = 7;
+int rainPin = A0;
 
 // moisture
+int moisturePin = A2;
 const double AirValue = 630;
 const double WaterValue = 260;
+
 // light
+int lightPin = A1;
 const double DarkValue = 840;
 const double LightValue = 2;
 
 // sensors -------------------------
 M2M_LM75A lm75a;
 Adafruit_BMP085 bmp180;
-DHT dht11(humidityPin, DHT11);
-
 
 void setup() {
     lm75a.begin();
     bmp180.begin();
     dht11.begin();
+
+    pinMode(rainPowerPin, OUTPUT);
 
     while (!Serial); // wait for serial monitor  
     Serial.begin(115200);
@@ -44,11 +52,16 @@ void loop() {
     moisture = (1 - ((moisture - WaterValue) / (AirValue - WaterValue))) * 100;
     
     float light = analogRead(lightPin);
-    Serial.println(light);
     light = (1 - ((light - LightValue) / (DarkValue - LightValue))) * 100;
-  
+
+    digitalWrite(rainPowerPin, HIGH);
+    delay(1);
+    float rain = analogRead(rainPin);
+    rain = (rain / WetValue) * 100;
+    digitalWrite(rainPowerPin, LOW);
+    
     // json object
-    const int capacity = JSON_OBJECT_SIZE(7);
+    const int capacity = JSON_OBJECT_SIZE(8);
     StaticJsonDocument<capacity> json;
 
     json["temp1"] = temp1;
@@ -58,6 +71,7 @@ void loop() {
     json["humidity"] = humidity;
     json["moisture"] = moisture;
     json["light"] = light;
+    json["rain"] = rain;
 
     serializeJson(json, Serial);
 
